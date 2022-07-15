2b8c4174006e8fbb67b4b9a7f23908a3835c9239
harish4459 <harish.sunny9@gmail.com>
2022-07-15 18:02:41 +0530

    Day5

---
+++ Day5-Longest_palindrome.py
@@ -0,0 +1,34 @@
+Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+
+Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
+
+ 
+
+Example 1:
+
+Input: s = "abccccdd"
+Output: 7
+Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+Example 2:
+
+Input: s = "a"
+Output: 1
+Explanation: The longest palindrome that can be built is "a", whose length is 1.
+ 
+
+Constraints:
+
+1 <= s.length <= 2000
+s consists of lowercase and/or uppercase English letters only.
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        ans = 0 
+        
+        for v in collections.Counter(s).values():
+            ans += v//2 * 2
+            
+            if ans % 2 == 0 and v % 2 == 1:
+                ans +=1
+        return ans
+        
