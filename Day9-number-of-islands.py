d02673c96023c91dd178cec3bc4581f286f064f5
harish4459 <harish.sunny9@gmail.com>
2022-07-18 11:02:29 +0530

    Day9

--- Day9-number-of-islands.py
+++ Day9-number-of-islands.py
@@ -84,12 +84,12 @@ class Solution:
             visit.add((r,c))
             q.append((r,c))
             while q:
-                row, col = q.popleft() # for DFS solution you can change q.popleft() to q.pop() 
+                row, col = q.popleft()
                 dir = [[1, 0], [-1, 0], [0, 1], [0, -1]]
                 
                 for dr, dc in dir:
                     r, c = row + dr, col + dc
-                    if( r in range(rows) and c in range(cols and grid[r,c] == "1" and (r,c) not in visit):
+                    if( r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r,c) not in visit):
                        q.append((r, c))
                        visit.add((r, c))
                 
@@ -98,8 +98,8 @@ class Solution:
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1" and (r,c) not in visit:
-                bfs(r,c)
-                islands += 1
+                    bfs(r,c)
+                    islands += 1
                 
         return islands
     
